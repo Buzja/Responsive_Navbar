99915a55a3caea4ee0395a5ffde6f7e4380a39c0
Buzja <buzanov1998152@mail.ru>
2018-06-23 02:02:07 +0300

    ok last commit

--- js/index.js
+++ js/index.js
@@ -3,20 +3,6 @@ const nav = document.querySelector(".sero_navbar");
 const arrows = document.querySelectorAll(".navbar_content__item_trigger");
 const body_el = document.body;
 
-// function openElementFunc(e){
-//     const elem_to_open = e.target.parentNode.nextElementSibling;
-//     console.log(elem_to_open.getAttribute("isopen"));
-    
-//     if(elem_to_open.getAttribute("isopen") == "true"){
-//         elem_to_open.setAttribute("isopen",false);
-//         elem_to_open.style.display = "none";
-//     }
-//     else{
-//         elem_to_open.setAttribute("isopen",true);
-//         elem_to_open.style.display = "block";
-//     }    
-// }
-
 function openElementFunc(e){
     const elem_to_open = e.target.parentNode.nextElementSibling;
     elem_to_open.classList.toggle("mobile_active");
